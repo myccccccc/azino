b306d20ce2a60fcdb0584b0e5f6e83f5b2dee326
mayingchun <mayingchun321@gmail.com>
2023-02-28 20:28:55 +0800

    fix

--- clang-format.py
+++ clang-format.py
@@ -1,7 +1,7 @@
 import os
 import os.path
 
-format_dir = ["example", "include", "sdk", "service", "storage", "txindex", "txplanner", "benchmark/dummy_bench"]
+format_dir = ["example", "include", "sdk", "service", "storage", "txindex", "txplanner", "benchmark"]
 path = os.getcwd()
 
 for p in format_dir:
