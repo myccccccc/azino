9e896cac98846d12be4f78fc68fa82f59acb72fd
mayingchun <mayingchun321@gmail.com>
2023-03-01 12:19:45 +0800

    fix

--- clang-format.py
+++ clang-format.py
@@ -8,7 +8,7 @@ for p in format_dir:
     p = path + '/' + p
     for root, dirs, files in os.walk(p):
         for name in files:
-            if (name.endswith(".h")) or (name.endswith(".cpp")):
+            if (name.endswith(".h")) or (name.endswith(".cpp")) or (name.endswith(".cc")):
                 localpath = root + '/' + name
                 print(localpath)
                 os.system("clang-format -i %s" % (localpath))
